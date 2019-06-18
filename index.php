6902fc23c601b86e66cb0c6b02f90909b16f598a
Bagdaulet <51380205+sagynov@users.noreply.github.com>
2019-06-18 10:35:28 +0500

    index.php file

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Cloud Mesasge</title>
+</head>
+<body>
+<br>
+<button id="signIn" class="btn btn-primary">Sign in with Google</button>
+<br>
+<pre id="object"></pre>
+<ul id="list"></ul>
+<div id="sms"></div>
+    <!-- Jquery  -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    
+    <script src="https://www.gstatic.com/firebasejs/6.2.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/6.2.0/firebase-auth.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/6.2.0/firebase-database.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/6.2.0/firebase-messaging.js"></script>
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+        https://firebase.google.com/docs/web/setup#config-web-app -->   
+
+    <script src="main.js"></script>
+</body>
+</html>
